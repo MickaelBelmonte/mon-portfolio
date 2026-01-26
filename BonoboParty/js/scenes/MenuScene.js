4127f2f7f8aba4ae38b801ae2204721f2621d196
Mickael Belmonte <belmonte.mickael@gmail.com>
2026-01-26 17:24:09 +0100

    Update MenuScene.js

--- BonoboParty/js/scenes/MenuScene.js
+++ BonoboParty/js/scenes/MenuScene.js
@@ -4,80 +4,31 @@ class MenuScene extends Phaser.Scene {
   }
 
   create() {
-    const centerX = this.cameras.main.width / 2;
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
 
-    this.add.text(centerX, 120, 'BONOBO PARTY', {
+    this.cameras.main.setBackgroundColor('#102018');
+
+    this.add.text(width / 2, 120, 'Bonobo Party', {
       fontSize: '48px',
-      fill: '#ffdd55'
+      fill: '#ffffff'
     }).setOrigin(0.5);
 
-    const createText = this.add.text(centerX, 260, '[ Créer une partie ]', {
-      fontSize: '28px',
-      fill: '#ffffff'
-    }).setOrigin(0.5).setInteractive();
-
-    const joinText = this.add.text(centerX, 320, '[ Rejoindre une partie ]', {
-      fontSize: '24px',
-      fill: '#cccccc'
-    }).setOrigin(0.5).setInteractive();
-
-    this.infoText = this.add.text(centerX, 420, '', {
+    this.add.text(width / 2, 180, 'Multijoueur 2 à 4 joueurs', {
       fontSize: '20px',
-      fill: '#ffffff'
+      fill: '#ffdd88'
     }).setOrigin(0.5);
 
-    // Hover
-    createText.on('pointerover', () => createText.setFill('#ffff88'));
-    createText.on('pointerout', () => createText.setFill('#ffffff'));
+    const playBtn = this.add.text(width / 2, height / 2, '[ Rejoindre la jungle ]', {
+      fontSize: '28px',
+      fill: '#ffdd55'
+    }).setOrigin(0.5).setInteractive();
 
-    joinText.on('pointerover', () => joinText.setFill('#ffff88'));
-    joinText.on('pointerout', () => joinText.setFill('#cccccc'));
+    playBtn.on('pointerover', () => playBtn.setFill('#ffee88'));
+    playBtn.on('pointerout', () => playBtn.setFill('#ffdd55'));
 
-    // Clicks
-    createText.on('pointerdown', () => this.handleCreateRoom());
-    joinText.on('pointerdown', () => {
-      const code = prompt('Code de la partie ?');
-      if (code) this.handleJoinRoom(code.trim().toUpperCase());
+    playBtn.on('pointerdown', () => {
+      this.scene.start('LobbyScene');
     });
   }
-
-  async handleCreateRoom() {
-    try {
-      const { roomCode, roomRef } = await createRoom();
-
-      this.registry.set('roomCode', roomCode);
-      this.registry.set('roomRef', roomRef);
-
-      const playerId = 'host_' + Math.floor(Math.random() * 99999);
-      this.registry.set('playerId', playerId);
-
-      await roomRef.child('players/' + playerId).set({
-        joinedAt: Date.now(),
-        ready: false
-      });
-
-      this.scene.start('LobbyScene');
-
-    } catch (e) {
-      this.infoText.setText('Erreur création partie');
-      console.error(e);
-    }
-  }
-
-  async handleJoinRoom(code) {
-    try {
-      const { roomRef, playerId } = await joinRoom(code);
-
-      this.registry.set('roomCode', code);
-      this.registry.set('roomRef', roomRef);
-      this.registry.set('playerId', playerId);
-
-      this.scene.start('LobbyScene');
-
-    } catch (e) {
-      this.infoText.setText('Partie introuvable');
-      console.error(e);
-    }
-  }
 }
-
