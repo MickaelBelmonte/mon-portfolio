655537a766074fee45c0bda301b43b2e6eb71ee6
Mickael Belmonte <belmonte.mickael@gmail.com>
2026-01-26 11:11:31 +0100

    Update main.js

--- BonoboParty/js/main.js
+++ BonoboParty/js/main.js
@@ -3,17 +3,22 @@ const config = {
   width: 800,
   height: 600,
   backgroundColor: '#1a2f1a',
+
   physics: {
     default: 'arcade',
-    arcade: { gravity: { y: 0 }, debug: false }
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
   },
-scene: [
-  BootScene,
-  PreloadScene,
-  MenuScene,
-  LobbyScene,
-  GameScene
-]
+
+  scene: [
+    BootScene,
+    PreloadScene,
+    MenuScene,
+    LobbyScene,
+    GameScene
+  ]
 };
 
 const game = new Phaser.Game(config);
