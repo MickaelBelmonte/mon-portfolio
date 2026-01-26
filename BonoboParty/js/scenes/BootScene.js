f99060dd427a20f29c0aa4be748363b8b5c14e5a
Mickael Belmonte <belmonte.mickael@gmail.com>
2026-01-26 17:22:50 +0100

    Update BootScene.js

--- BonoboParty/js/scenes/BootScene.js
+++ BonoboParty/js/scenes/BootScene.js
@@ -3,20 +3,7 @@ class BootScene extends Phaser.Scene {
     super('BootScene');
   }
 
-  preload() {
-    // Exemple : charger un logo
-    // this.load.image('logo', 'js/assets/logo.png');
-  }
-
   create() {
-    // Logo :
-    // const logo = this.add.image(400, 300, 'logo').setScale(0.5);
-
-    // Petite transition stylée
-    this.cameras.main.fadeIn(400, 0, 0, 0);
-
-    this.time.delayedCall(600, () => {
-      this.scene.start('PreloadScene');
-    });
+    this.scene.start('PreloadScene');
   }
 }
