839baddd9f4130ce33cc9ac2de436a04a41c2ae7
Mickael Belmonte <belmonte.mickael@gmail.com>
2026-01-26 10:49:36 +0100

    Create main.js

---
+++ BonoboParty/js/main.js
@@ -0,0 +1,20 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#1a2f1a',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scene: [
+    BootScene,
+    PreloadScene,
+    MenuScene
+  ]
+};
+
+const game = new Phaser.Game(config);
