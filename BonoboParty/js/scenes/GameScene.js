bfaf23b68611025877cf0e281961be116ecb8bdf
Mickael Belmonte <belmonte.mickael@gmail.com>
2026-01-26 11:08:22 +0100

    Update GameScene.js

--- BonoboParty/js/scenes/GameScene.js
+++ BonoboParty/js/scenes/GameScene.js
@@ -7,13 +7,16 @@ class GameScene extends Phaser.Scene {
     const centerX = this.cameras.main.width / 2;
     const centerY = this.cameras.main.height / 2;
 
-    this.add.text(centerX, 80, 'Mini‑jeu prototype', {
+    this.roomRef = this.registry.get('roomRef');
+    this.playerId = this.registry.get('playerId');
+
+    this.add.text(centerX, 80, 'Mini‑jeu : Prototype', {
       fontSize: '32px',
       fill: '#ffffff'
     }).setOrigin(0.5);
 
-    this.add.text(centerX, 140, '"Course aux noix", etc.', {
-      fontSize: '18px',
+    this.add.text(centerX, 140, 'Ici tu vas coder ton premier mini‑jeu', {
+      fontSize: '20px',
       fill: '#dddddd'
     }).setOrigin(0.5);
 
@@ -22,17 +25,14 @@ class GameScene extends Phaser.Scene {
       fill: '#ffdd55'
     }).setOrigin(0.5);
 
-    this.cameras.main.setBackgroundColor('#123322');
+    // Exemple : écoute Firebase pour synchroniser les joueurs
+    listenRoom(this.roomRef, (data) => {
+      // Tu pourras synchroniser les positions, scores, etc.
+      console.log('Game update:', data);
+    });
 
     this.input.once('pointerdown', () => {
-      this.scene.transition({
-        target: 'MenuScene',
-        duration: 500,
-        moveBelow: true,
-        onUpdate: (progress) => {
-          this.cameras.main.setAlpha(1 - progress);
-        }
-      });
+      this.scene.start('MenuScene');
     });
   }
 }
